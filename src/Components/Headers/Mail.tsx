81cb0d44f021050cc1b9894766a71ee0eaa2fe71
Siam Ahnaf <siamahnaf198@gmail.com>
2023-09-13 22:11:16 +0600

    Adding Mailing link

--- src/Components/Headers/Mail.tsx
+++ src/Components/Headers/Mail.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Mail = () => {
     return (
         <div className="col-span-3">
-            <Link href="" className="font-medium hover:text-secondary transition-all opacity-80">
+            <Link href="mailto:info@cabbageweb.com" className="font-medium hover:text-secondary transition-all opacity-80">
                 info@cabbageweb.com
             </Link>
         </div>
