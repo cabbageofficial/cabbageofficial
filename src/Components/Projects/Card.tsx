1d46ef01a59c5ede1edb69c52c98572888e6a275
Siam Ahnaf <siamahnaf198@gmail.com>
2023-09-15 20:22:51 +0600

    Initial Commit

--- src/Components/Projects/Card.tsx
+++ src/Components/Projects/Card.tsx
@@ -4,34 +4,52 @@ import Link from "next/link";
 //Data
 const data = [
     {
-        image: "/projects/project-1.jpg",
-        name: "Creative & digital web design studio.",
-        type: "Magazine, Photography",
+        image: "/projects/project-final-1.png",
+        name: "Creative custom online e-commerce website",
+        type: "E-commerce, Nekmart",
+        url: "https://nekmart.com/",
+        width: 940,
+        height: 1140
     },
     {
-        image: "/projects/project-2.jpg",
-        name: "Creative & digital web design studio.",
-        type: "Magazine, Photography"
+        image: "/projects/project-final-2.png",
+        name: "A nextjs e-commerce frontend template",
+        type: "E-commerce, Ebuy",
+        url: "https://ebuy.cabbageweb.com/",
+        width: 1200,
+        height: 1140
     },
     {
-        image: "/projects/project-3.jpg",
-        name: "Creative & digital web design studio.",
-        type: "Magazine, Photography"
+        image: "/projects/project-final-3.png",
+        name: "A movies and book collection website!",
+        type: "Entertainment, Flimybook",
+        url: "https://flimybook.siamahnaf.com/",
+        width: 1540,
+        height: 1140
     },
     {
-        image: "/projects/project-10.jpg",
-        name: "Creative & digital web design studio.",
-        type: "Magazine, Photography"
+        image: "/projects/project-final-4.png",
+        name: "A online greetings booking website!",
+        type: "Real estate, We do greetings",
+        url: "https://www.wedogreetings.co.uk/",
+        width: 940,
+        height: 1140
     },
     {
-        image: "/projects/project-11.jpg",
-        name: "Creative & digital web design studio.",
-        type: "Magazine, Photography"
+        image: "/projects/project-final-5.png",
+        name: "A progressive blog and news site!",
+        type: "Blog & News, Siam's blog",
+        url: "https://blog.siamahnaf.com/",
+        width: 1200,
+        height: 1140
     },
     {
         image: "/projects/project-12.jpg",
         name: "Creative & digital web design studio.",
-        type: "Magazine, Photography"
+        type: "Magazine, Photography",
+        url: "/",
+        width: 770,
+        height: 570
     }
 ]
 
@@ -42,8 +60,8 @@ const Card = () => {
                 {data.map((item, i) => (
                     <div key={i} className={`${i === 2 || (i > 2 && (i - 2) % 3 === 0) ? "col-span-2 w-[70%] mx-auto" : ''
                         } border-b border-solid border-gray-100`}>
-                        <Link href="/" className="group overflow-hidden block">
-                            <Image src={item.image} alt={item.name} width={470} height={570} className="w-full transition-all duration-200 ease-[cubic-bezier(0.4, 0, 0.2, 1)] group-hover:scale-105" />
+                        <Link href={item.url} className="group overflow-hidden block">
+                            <Image src={item.image} alt={item.name} width={item.width} height={item.height} className="w-full transition-all duration-200 ease-[cubic-bezier(0.4, 0, 0.2, 1)] group-hover:scale-105" />
                             <div className="px-2 py-3">
                                 <p className="text-sm">{item.type}</p>
                                 <h4 className="text-lg font-bold">{item.name}</h4>
